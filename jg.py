a6e6874d23a15103f6feb967b6993680ed010931
Gustavo Hernández <tavo_razielQhotmail.com>
2024-01-18 14:56:34 -0600

    juego, piedra papel o tijera, y refactorizacion del codigo

---
+++ jg.py
@@ -0,0 +1,74 @@
+import random
+
+options = ('piedra','papel','tijera')
+
+computer_wins = 0
+user_wins = 0
+
+round = 1
+
+while  True:
+
+  print('*' * 10)
+  print('ROUND', round)
+  print('*' * 10)
+
+  print('conteo computador', computer_wins)
+  print('conteo usuario', user_wins)
+
+
+  user_option = input("piedra, papel o tijera =>")
+  user_option = user_option.lower()
+
+  round += 1
+
+  
+  if not user_option in options:
+    print('Esa opcion no es valida')
+    continue
+  
+  computer_option = random.choice(options)
+  
+  print('User option =>', user_option)
+  print('Computer option =>', computer_option)
+  
+  if user_option == computer_option:
+    print("Empate!")
+  elif user_option == "piedra":
+    if computer_option == "tijera":
+      print("piedra gana a tijera")
+      print("user gano!")
+      user_wins += 1
+    else:
+      print("papel gana a piedra")
+      print("computer gano!")
+      computer_wins += 1
+  elif user_option == "papel":
+    if computer_option == "piedra":
+      print("papel le gana a piedra")
+      print("user gano!")
+      user_wins += 1
+    else:
+      print("tijera le gana a papel")
+      print("computer gano!")
+      computer_wins += 1
+  elif user_option == "tijera":
+    if computer_option == "papel":
+      print("tijera le gana a papel")
+      print("user gano!")
+      user_wins += 1
+    else:
+      print("piedra le gana a tijera")
+      print("computer gano!")
+      computer_wins += 1
+
+  if computer_wins == 2:
+    print('El computador gano!!')
+    break
+
+  if user_wins == 2:
+    print('El usuario gano es pro!!')
+    break
+    
+  
+
